b1075862ca72e44c7e5d43a17dc03618e6dc31aa
0xPr0f <0xprofoz@gmail.com>
2022-05-01 18:52:03 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br />
+      <br />
+      <div className="menu-container">
+        <div className="button-container">
+          <button id="0" className="button"></button>
+          <button id="1" className="button"></button>
+          <button id="2" className="button"></button>
+          <button id="3" className="button"></button>
+          <button id="4" className="button"></button>
+          <button id="5" className="button"></button>
+          <button id="6" className="button"></button>
+          <button id="7" className="button"></button>
+          <button id="8" className="button"></button>
+          <button id="9" className="button"></button>
+          <button id="10" className="button"></button>
+          <button id="11" className="button"></button>
+          <button id="12" className="button"></button>
+          <button id="13" className="button"></button>
+          <button id="14" className="button"></button>
+          <button id="15" className="button"></button>
+          <button id="16" className="button"></button>
+          <button id="17" className="button"></button>
+          <button id="18" className="button"></button>
+          <button id="19" className="button"></button>
+          <button id="20" className="button"></button>
+          <button id="21" className="button"></button>
+          <button id="22" className="button"></button>
+          <button id="23" className="button"></button>
+          <button id="24" className="button"></button>
+          <button id="25" className="button"></button>
+          <button id="26" className="button"></button>
+          <button id="27" className="button"></button>
+          <button id="28" className="button"></button>
+          <button id="29" className="button"></button>
+          <button id="30" className="button"></button>
+          <button id="31" className="button"></button>
+          <button id="32" className="button"></button>
+          <button id="33" className="button"></button>
+          <button id="34" className="button"></button>
+          <button id="35" className="button"></button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/index.css";
+import App from "./App";
+import { MoralisProvider } from "react-moralis";
+import reportWebVitals from "./reportWebVitals";
+import { NotificationProvider } from "web3uikit";
+const Moralis = require("moralis");
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const MORALIS_SERVER_URL = "https://pzp74zw8ntnj.usemoralis.com:2053/server";
+const MORALIS_APP_ID = "IfaV72J3L6zxbUkXO9ioPABMBCYPNBfxts7Ll3rG";
+Moralis.start({ serverUrl: MORALIS_SERVER_URL, appId: MORALIS_APP_ID });
 root.render(
   <React.StrictMode>
-    <App />
+    <MoralisProvider appId={MORALIS_APP_ID} serverUrl={MORALIS_SERVER_URL}>
+      <NotificationProvider>
+        <App />
+      </NotificationProvider>
+    </MoralisProvider>
   </React.StrictMode>
 );
 
